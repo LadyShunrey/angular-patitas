fe8e12bcfea38ddc9778e37bb6e095dac34e0a1f
Melisa Burlando <melisa.burlando@gmail.com>
2022-11-28 13:30:07 -0300

    agrego todos los componentes y la estructura del proyecto armada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AdoptionsComponent } from './adoptions/adoptions.component';
+import { AppComponent } from './app.component';
+import { RescuersComponent } from './rescuers/rescuers.component';
+import { StoreComponent } from './store/store.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: AppComponent
+  },
+  {
+    path: 'adoptions',
+    component: AdoptionsComponent
+  },
+  {
+    path: 'store',
+    component: StoreComponent
+  },
+  {
+    path: 'rescuers',
+    component: RescuersComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+import { AdoptionsComponent } from './adoptions/adoptions.component';
+import { RescuersComponent } from './rescuers/rescuers.component';
+import { AboutComponent } from './about/about.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreComponent,
+    AdoptionsComponent,
+    RescuersComponent,
+    AboutComponent,
+    ProductListComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
